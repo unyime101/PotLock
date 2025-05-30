6c6897a88fc689175a5967b521ea0ac9c5e68bce
unyime101 <unyime@gmail.com>
2025-05-30 23:14:37 +0100

    started sign up

---
+++ utils/signup.py
@@ -0,0 +1,8 @@
+from ..config.db_config import db_con
+
+def sign_up():
+    email = input(print("Please enter your email address: "))
+    Name = input(print("Please enter your full name: "))
+    phonenumber = input(print("Please enter your phone number: "))
+    passw = input(print("Please enter a password: "))
+    
