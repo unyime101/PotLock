4dc537f6ef17885ae590d1a5c5ae5a0e6b72faff
unyime101 <unyime@gmail.com>
2025-06-05 14:06:48 +0100

    Null

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from utils.signup import sign_up
 from config.db_test import get_users, get_tables
 from utils.login import login
-usr_id = 0 # will be used further to select all user details linked to this id
+ 
 #get_tables()
-usr_id = login()
+usr_id = login()# will be used further to select all user details linked to this id
 print(usr_id)
 #sign_up()
 #get_users()
