81563b19b9c647e838704e42a00ae0040e8991b7
unyime101 <unyime@gmail.com>
2025-08-07 20:32:19 +0100

    Removed config fiiles

--- config/db_config.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def db_con():# connectiuon method, will be used all throuh proj
-    return mysql.connector.connect( 
-        host="127.0.0.1",
-        user="root",
-        password="@Glory2003",
-        database="potlock"
-        )
